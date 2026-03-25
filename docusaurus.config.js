482708e7450510bfe5a766e47843ff3ed78afa81
jdwfn <chenwei_018@outlook.com>
2026-03-25 19:09:21 +0800

    update files to remote

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://IvyW-XBO.github.io/testers.ai-cli-docs/',
+  url: 'https://IvyW-XBO.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
